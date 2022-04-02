7bc03ac3c6663b0d7b33a48b4346223d075442ed
Rob Herley <robherley@github.com>
2022-04-01 23:32:35 -0400

    JR, CALL, RET, RETI and other cleanup

--- pkg/cpu/cpu.go
+++ pkg/cpu/cpu.go
@@ -44,28 +44,22 @@ func (c *CPU) Fetch16() uint16 {
 
 // ValueOf resolves the given instruction symbol's value based on CPU state
 func (c *CPU) ValueOf(operand *instructions.Operand) uint16 {
-	var val uint16
-
 	switch symbol := (*operand).Symbol.(type) {
 	case instructions.Data:
-		switch symbol {
-		case instructions.D8, instructions.A8:
-			return uint16(c.Fetch8())
-		case instructions.D16, instructions.A16:
+		if operand.Is16() {
 			return c.Fetch16()
-		default:
-			panic(fmt.Errorf("invalid symbol: %v", symbol))
+		} else {
+			return uint16(c.Fetch8())
 		}
 	case instructions.Register:
-		val = c.Registers.Get(symbol)
+		val := c.Registers.Get(symbol)
 		if operand.Deref {
 			val = uint16(c.Read8(val))
 		}
+		return val
 	case byte:
-		val = uint16(symbol)
+		return uint16(symbol)
 	default:
 		panic(fmt.Errorf("invalid operand type: %T", symbol))
 	}
-
-	return val
 }
--- pkg/cpu/process.go
+++ pkg/cpu/process.go
@@ -11,34 +11,77 @@ import (
 
 // Process an instruction for a given mnemonic, returns number of ticks
 func (c *CPU) Process(in *instructions.Instruction) byte {
+	ops := in.Operands
+
 	switch in.Mnemonic {
 	case instructions.NOP:
-		return c.NOP(in)
+		return c.NOP(ops)
 	case instructions.JP:
-		return c.JP(in)
+		return c.JP(ops)
+	case instructions.JR:
+		return c.JR(ops)
+	case instructions.CALL:
+		return c.CALL(ops)
+	case instructions.RET:
+		return c.RET(ops)
+	case instructions.RETI:
+		return c.RETI(ops)
 	case instructions.DI:
-		return c.DI(in)
+		return c.DI(ops)
 	case instructions.EI:
-		return c.EI(in)
+		return c.EI(ops)
 	case instructions.XOR:
-		return c.XOR(in)
+		return c.XOR(ops)
 	case instructions.LD:
-		return c.LD(in)
+		return c.LD(ops)
 	case instructions.LDH:
-		return c.LDH(in)
+		return c.LDH(ops)
 	default:
 		panic(fmt.Errorf("instruction not implemented: %s", in.Mnemonic))
 	}
 }
 
+// jumper is a helper method for jump operations (JP, JR, CALL, etc)
+func (c *CPU) jumper(ops []instructions.Operand, relative, pushPC bool) byte {
+	var addr uint16
+
+	// check if conditional jump
+	if len(ops) > 1 {
+		condition, _ := ops[0].Symbol.(instructions.Condition)
+		if !c.Registers.IsCondition(condition) {
+			// condition did not pass, so just return
+			return 4
+		}
+		addr = c.ValueOf(&ops[1])
+	} else {
+		// doesn't have condition, resolve the value
+		addr = c.ValueOf(&ops[0])
+	}
+
+	// push program counter, used for CALL
+	if pushPC {
+		c.StackPush16(c.Registers.PC)
+	}
+
+	// cast and add signed data for relative jump, used for JR
+	if relative {
+		rel := int8(addr & 0xFF)
+		addr = c.Registers.PC + uint16(rel)
+	}
+
+	c.Registers.PC = addr
+
+	return 4
+}
+
 // NOP: No operation
-func (c *CPU) NOP(in *instructions.Instruction) byte {
+func (c *CPU) NOP(ops []instructions.Operand) byte {
 	return 4
 }
 
 // INC: increment register
-func (c *CPU) INC(in *instructions.Instruction) byte {
-	reg, _ := in.Operands[0].Symbol.(instructions.Register)
+func (c *CPU) INC(ops []instructions.Operand) byte {
+	reg, _ := ops[0].Symbol.(instructions.Register)
 
 	val := c.Registers.Get(reg)
 	c.Registers.Set(reg, val+1)
@@ -47,8 +90,8 @@ func (c *CPU) INC(in *instructions.Instruction) byte {
 }
 
 // DEC: decrement register
-func (c *CPU) DEC(in *instructions.Instruction) byte {
-	reg, _ := in.Operands[0].Symbol.(instructions.Register)
+func (c *CPU) DEC(ops []instructions.Operand) byte {
+	reg, _ := ops[0].Symbol.(instructions.Register)
 
 	val := c.Registers.Get(reg)
 	c.Registers.Set(reg, val-1)
@@ -56,38 +99,73 @@ func (c *CPU) DEC(in *instructions.Instruction) byte {
 	return 4
 }
 
-// JP: jump to address
-func (c *CPU) JP(in *instructions.Instruction) byte {
-	// check if conditional jump
-	if len(in.Operands) > 1 {
-		condition, _ := in.Operands[0].Symbol.(instructions.Condition)
-		if c.Registers.IsCondition(condition) {
-			// condition passed, so jump to resolved value
-			c.Registers.PC = c.ValueOf(&in.Operands[1])
+// JP: jump to address (and check condition)
+func (c *CPU) JP(ops []instructions.Operand) byte {
+	// call jumper helper
+	// not relative
+	// don't push PC to stack
+	return c.jumper(ops, false, false)
+}
+
+// JR: jump to relative address (and check condition)
+func (c *CPU) JR(ops []instructions.Operand) byte {
+	// call jumper helper
+	// relative
+	// don't push PC to stack
+	return c.jumper(ops, true, false)
+}
+
+// CALL: push address of next instruction onto stack (and check condition)
+func (c *CPU) CALL(ops []instructions.Operand) byte {
+	// call jumper helper
+	// not relative
+	// push PC to stack
+	return c.jumper(ops, false, true)
+}
+
+// RET: pop two bytes from stack & jump to that address (and check condition)
+func (c *CPU) RET(ops []instructions.Operand) byte {
+	addr := c.StackPop16()
+
+	if len(ops) > 0 {
+		condition, _ := ops[0].Symbol.(instructions.Condition)
+		if !c.Registers.IsCondition(condition) {
+			// condition did not pass, so just return
+			return 4
 		}
-	} else {
-		// doesn't have condition, resolve the value
-		c.Registers.PC = c.ValueOf(&in.Operands[0])
 	}
+
+	c.Registers.PC = addr
+
+	return 4
+}
+
+// RETI: pop two bytes from stack & jump to that address then enable interrupts
+func (c *CPU) RETI(ops []instructions.Operand) byte {
+	// just a RET
+	c.RET(ops)
+
+	// and re-enable interrupts
+	c.IME = true
 
 	return 4
 }
 
 // DI: disables interrupts
-func (c *CPU) DI(in *instructions.Instruction) byte {
+func (c *CPU) DI(ops []instructions.Operand) byte {
 	c.IME = false
 	return 4
 }
 
 // EI: enables interrupts
-func (c *CPU) EI(in *instructions.Instruction) byte {
+func (c *CPU) EI(ops []instructions.Operand) byte {
 	c.IME = true
 	return 4
 }
 
 // XOR: logical exclusive OR with register A
-func (c *CPU) XOR(in *instructions.Instruction) byte {
-	value := c.ValueOf(&in.Operands[0])
+func (c *CPU) XOR(ops []instructions.Operand) byte {
+	value := c.ValueOf(&ops[0])
 	c.Registers.A ^= bits.Lo(value)
 
 	// set zero flag if result is zero
@@ -104,12 +182,12 @@ func (c *CPU) XOR(in *instructions.Instruction) byte {
 }
 
 // LD: puts values from one operand into another
-func (c *CPU) LD(in *instructions.Instruction) byte {
-	numOps := len(in.Operands)
+func (c *CPU) LD(ops []instructions.Operand) byte {
+	numOps := len(ops)
 
 	// special case instruction for 0xF8
 	if numOps == 3 {
-		r8 := c.ValueOf(&in.Operands[2])
+		r8 := c.ValueOf(&ops[2])
 
 		// half carry (4 bits)
 		setH := (c.Registers.SP&0xF)+(r8&0xF) > 0xF
@@ -132,8 +210,8 @@ func (c *CPU) LD(in *instructions.Instruction) byte {
 		return 4
 	}
 
-	dst := &in.Operands[0]
-	src := &in.Operands[1]
+	dst := &ops[0]
+	src := &ops[1]
 
 	srcData := c.ValueOf(src)
 
@@ -151,18 +229,18 @@ func (c *CPU) LD(in *instructions.Instruction) byte {
 	}
 
 	// check if any HL+ or HL-, and adjust
-	for i := range in.Operands {
-		if in.Operands[i].Symbol != instructions.HL {
+	for i := range ops {
+		if ops[i].Symbol != instructions.HL {
 			continue
 		}
 
 		hl := c.Registers.Get(instructions.HL)
 
-		if in.Operands[i].Inc {
+		if ops[i].Inc {
 			c.Registers.Set(instructions.HL, hl+1)
 		}
 
-		if in.Operands[i].Dec {
+		if ops[i].Dec {
 			c.Registers.Set(instructions.HL, hl-1)
 		}
 	}
@@ -171,18 +249,18 @@ func (c *CPU) LD(in *instructions.Instruction) byte {
 }
 
 // LDH: loads/sets A from 8-bit signed data
-func (c *CPU) LDH(in *instructions.Instruction) byte {
-	first := in.Operands[0].Symbol
-	second := in.Operands[1].Symbol
+func (c *CPU) LDH(ops []instructions.Operand) byte {
+	first := ops[0].Symbol
+	second := ops[1].Symbol
 
 	if first == instructions.A && second == instructions.A8 {
 		// LDH A (a8), alternate mnemonic is LD A,($FF00+a8)
-		a8 := c.ValueOf(&in.Operands[1])
+		a8 := c.ValueOf(&ops[1])
 		c.Registers.A = c.Read8(0xFF00 | a8)
 
 	} else if first == instructions.A8 && second == instructions.A {
 		// LDH (a8) A, alternate mnemonic is LD ($FF00+a8),A
-		a8 := c.ValueOf(&in.Operands[0])
+		a8 := c.ValueOf(&ops[0])
 		c.Write8(0xFF00|a8, c.Registers.A)
 	}
 
@@ -190,22 +268,23 @@ func (c *CPU) LDH(in *instructions.Instruction) byte {
 }
 
 // POP: pops a two byte value off the stack
-func (c *CPU) POP(in *instructions.Instruction) byte {
+func (c *CPU) POP(ops []instructions.Operand) byte {
 	val := c.StackPop16()
 
 	// special case for AF, protect last nibble for flags
-	if in.Operands[0].Symbol == instructions.AF {
+	if ops[0].Symbol == instructions.AF {
 		c.Registers.SetAF(val & 0xFFF0)
 	} else {
-		c.Registers.SetAF(val)
+		reg, _ := ops[0].Symbol.(instructions.Register)
+		c.Registers.Set(reg, val)
 	}
 
 	return 4
 }
 
 // PUSH: pushes a two byte value on the stacks
-func (c *CPU) PUSH(in *instructions.Instruction) byte {
-	val := c.ValueOf(&in.Operands[0])
+func (c *CPU) PUSH(ops []instructions.Operand) byte {
+	val := c.ValueOf(&ops[0])
 	c.StackPush16(val)
 
 	return 4
