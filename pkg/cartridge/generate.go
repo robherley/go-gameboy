d2cf7032178d7fca0df15c9b6cd78e07f59b6d26
Rob Herley <robherley@github.com>
2022-05-06 20:43:26 -0400

    rm string generators

--- pkg/cartridge/generate.go
+++
@@ -1,4 +0,0 @@
-package cartridge
-
-// Generates strings of constants to assist in printing/debugging
-//go:generate stringer -type=CartridgeType -output=zz_generated_strings.go
--- pkg/cpu/generate.go
+++
@@ -1,4 +0,0 @@
-package cpu
-
-// Generates strings of constants to assist in printing/debugging
-//go:generate stringer -type=InterruptType,InterruptMasterChange -output=zz_generated_strings.go
